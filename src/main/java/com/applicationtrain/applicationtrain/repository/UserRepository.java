14e1c1fe66fbb7384553040b75b24d69ce849b14
CRITELLI Alicia (SNCF VOYAGEURS / DIRECTION DE LIGNES D9306720AR / Sect Paris St Denis eq1 m) <9306720a@commun.ad.sncf.fr>
2023-03-21 14:09:43 +0100

    ok

--- src/main/java/com/applicationtrain/applicationtrain/repository/UserRepository.java
+++ src/main/java/com/applicationtrain/applicationtrain/repository/UserRepository.java
@@ -2,8 +2,18 @@ package com.applicationtrain.applicationtrain.repository;
 
 import com.applicationtrain.applicationtrain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    //  SELECT * FROM user WHERE gender = "F" AND train_number = 1890;
+
+    @Query("select u from User u where u.gender = 'F' AND u.train_number = 1850")
+    List<User> findFemaleByTrainNumber();
+    @Query("select u from User u where u.gender = 'M' AND u.train_number = 1850" )
+    List<User> findMaleByTrainNumber();
 }
