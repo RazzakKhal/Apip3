03d1eee1b36e6dc003a48c85e50a5b79f6e8b94d
KHALFALLAH Razzak (SNCF VOYAGEURS / ET TRACTION PRODUCTION LIGNE E / Eq conduite 4 Ligne E) <9506532m@commun.ad.sncf.fr>
2023-03-24 15:35:51 +0100

    security

--- src/main/java/com/applicationtrain/applicationtrain/service/AccueilService.java
+++ src/main/java/com/applicationtrain/applicationtrain/service/AccueilService.java
@@ -1,7 +1,11 @@
 package com.applicationtrain.applicationtrain.service;
 
 import com.applicationtrain.applicationtrain.entity.User;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface AccueilService {
     User userInscription(User user);
+
+
+
 }
--- src/main/java/com/applicationtrain/applicationtrain/service/AccueilServiceImpl.java
+++ src/main/java/com/applicationtrain/applicationtrain/service/AccueilServiceImpl.java
@@ -3,11 +3,15 @@ package com.applicationtrain.applicationtrain.service;
 import com.applicationtrain.applicationtrain.entity.User;
 import com.applicationtrain.applicationtrain.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+
 @Service
-public class AccueilServiceImpl implements AccueilService{
+public class AccueilServiceImpl implements AccueilService, UserDetailsService {
     @Autowired
     BCryptPasswordEncoder bCryptPasswordEncoder;
     @Autowired
@@ -17,5 +21,12 @@ public class AccueilServiceImpl implements AccueilService{
         user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
         userRepository.save(user);
         return user;
+    }
+
+    // methode qui récupere un utilisateur par son mail et renvoi un User de type UserDetail avec les informations d'authentification
+    @Override
+    public UserDetails loadUserByUsername(String mail){
+      User userFind = userRepository.findByMail(mail);
+        return new org.springframework.security.core.userdetails.User(userFind.getMail(), userFind.getPassword(), new ArrayList<>());
     }
 }
