c92bcd3312d8e5e19a1cb32a185a34993e7ad8e9
KHALFALLAH Razzak (SNCF VOYAGEURS / ET TRACTION PRODUCTION LIGNE E / Eq conduite 4 Ligne E) <9506532m@commun.ad.sncf.fr>
2023-03-27 14:27:39 +0200

    return an objet for token

--- src/main/java/com/applicationtrain/applicationtrain/service/AccueilService.java
+++ src/main/java/com/applicationtrain/applicationtrain/service/AccueilService.java
@@ -1,6 +1,7 @@
 package com.applicationtrain.applicationtrain.service;
 
 import com.applicationtrain.applicationtrain.entity.User;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
--- src/main/java/com/applicationtrain/applicationtrain/service/AccueilServiceImpl.java
+++ src/main/java/com/applicationtrain/applicationtrain/service/AccueilServiceImpl.java
@@ -5,6 +5,7 @@ import com.applicationtrain.applicationtrain.entity.User;
 import com.applicationtrain.applicationtrain.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
