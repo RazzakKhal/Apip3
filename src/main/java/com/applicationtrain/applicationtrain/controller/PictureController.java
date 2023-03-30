53c30cf2a23056f310d4d5753536fb8d4748ab12
KHALFALLAH Razzak (SNCF VOYAGEURS / ET TRACTION PRODUCTION LIGNE E / Eq conduite 4 Ligne E) <9506532m@commun.ad.sncf.fr>
2023-03-30 14:28:43 +0200

    return all photos off user when add photo

--- src/main/java/com/applicationtrain/applicationtrain/controller/PictureController.java
+++ src/main/java/com/applicationtrain/applicationtrain/controller/PictureController.java
@@ -1,22 +1,31 @@
 package com.applicationtrain.applicationtrain.controller;
 
 import com.applicationtrain.applicationtrain.entity.Picture;
+import com.applicationtrain.applicationtrain.entity.User;
 import com.applicationtrain.applicationtrain.repository.PictureRepository;
+import com.applicationtrain.applicationtrain.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 @CrossOrigin
 @RestController
 @RequestMapping("/picture")
 public class PictureController {
 
     @Autowired
+    private UserRepository userRepository;
+    @Autowired
     private PictureRepository pictureRepository;
 
     @PostMapping(value = "/addpicture")
-    public ResponseEntity<Picture> savePicture(@RequestBody Picture imageUrl){ // recupere l'URL de l'img par POST
+    public ResponseEntity<List<Picture>> savePicture(@RequestBody Picture imageUrl){ // recupere l'URL de l'img par POST
         Picture picture = new Picture(imageUrl.getLink(), imageUrl.getUser());
         pictureRepository.save(picture);
-        return ResponseEntity.ok(picture); //return une reponse HTTP 200
+        User user = userRepository.findByMail(imageUrl.getUser().getMail());
+
+        return ResponseEntity.ok(user.getPictures()); //return une reponse HTTP 200
     }
 }
--- src/main/java/com/applicationtrain/applicationtrain/repository/PictureRepository.java
+++ src/main/java/com/applicationtrain/applicationtrain/repository/PictureRepository.java
@@ -1,9 +1,15 @@
 package com.applicationtrain.applicationtrain.repository;
 
 import com.applicationtrain.applicationtrain.entity.Picture;
+import com.applicationtrain.applicationtrain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PictureRepository extends JpaRepository<Picture, Long> {
+
+
+
 }
