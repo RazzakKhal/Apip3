c8847c553a264dc62669ca4f2a28ffb0e29da9e4
CRITELLI Alicia (SNCF VOYAGEURS / DIRECTION DE LIGNES D9306720AR / Sect Paris St Denis eq1 m) <9306720a@commun.ad.sncf.fr>
2023-06-14 21:43:14 +0200

    refactoring

--- src/main/java/com/applicationtrain/applicationtrain/entity/LikeEntity.java
+++ src/main/java/com/applicationtrain/applicationtrain/entity/LikeEntity.java
@@ -3,11 +3,14 @@ package com.applicationtrain.applicationtrain.entity;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
 
+import java.util.Date;
+
 @Entity
 public class LikeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private Date date;
 
 @JsonBackReference(value ="user_likeSender")
     @ManyToOne(cascade = CascadeType.REFRESH)
@@ -26,6 +29,7 @@ public class LikeEntity {
     public LikeEntity(User likeSender, User likeReceiver) {
         this.likeSender = likeSender;
         this.likeReceiver = likeReceiver;
+        this.date = new Date();
     }
 
     public Long getId() {
@@ -34,6 +38,14 @@ public class LikeEntity {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
     }
 
     public User getLikeSender() {
